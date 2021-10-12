6c64b0759ecdd1b0c747744ecb8b9783e73a98db
Russell Warner <rewarner@troped.com>
2021-10-12 13:06:52 -0400

    Hardcasting two different renderers to two different <canvas> elements.

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,27 +2,40 @@ import * as THREE from 'three'
 
 const scene = new THREE.Scene()
 
+scene.background = new THREE.Color(0x3412ff)
+
 const camera = new THREE.PerspectiveCamera(
     75,
-    window.innerWidth / window.innerHeight,
+    // window.innerWidth / window.innerHeight,
+    1,
     0.1,
     1000
 )
 camera.position.z = 2
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const canvas1 = document.getElementById('cube') as HTMLCanvasElement
+const canvas2 = document.getElementById('clone') as HTMLCanvasElement
+
+// const renderer = new THREE.WebGLRenderer()
+const renderer1 = new THREE.WebGLRenderer({canvas:canvas1})
+renderer1.setSize(200, 200)
+const renderer2 = new THREE.WebGLRenderer({canvas:canvas2})
+renderer2.setSize(200, 200)
+
+// To return to full screen
+// renderer.setSize(window.innerWidth, window.innerHeight)
+// document.body.appendChild(renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
     color: 0xFF0043,
-    wireframe: true,
+    wireframe: false,
 })
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+/*
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
@@ -30,6 +43,7 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight)
     render()
 }
+*/
 
 function animate() {
     requestAnimationFrame(animate)
@@ -41,7 +55,8 @@ function animate() {
 }
 
 function render() {
-    renderer.render(scene, camera)
+    renderer1.render(scene, camera)
+    renderer2.render(scene, camera)
 }
 
 animate()
